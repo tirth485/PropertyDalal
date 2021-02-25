7d6833b3acf6bd6d0afc23413e96309f49923bf0
savanp0322 <savanp.0322@gmail.com>
2021-02-25 13:41:42 +0530

    Connector.php Added

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "remotemysql.com";
+$username = "UMHtrZaysp";
+$password = "3N3N6nsb7Z";
+$database = "UMHtrZaysp";
+// Create connection
+$conn = new mysqli($servername, $username, $password,$database);
+echo $_SERVER['HTTP_HOST'];
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?> 
